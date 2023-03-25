94835fe0623d46ab05adfba8d42866a6f42252e0
Victor Santos <vy.santos@live.com>
2023-03-25 14:32:53 -0300

    realizando testes de ponta a ponta

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,11 +1,15 @@
-import { test, expect } from "@playwright/test";
-import { faker } from "@faker-js/faker";
+import { test, expect, request } from "@playwright/test";
 
-test("deve cadastrar uma nova tarefa", async ({ page }) => {
+test("deve cadastrar uma nova tarefa", async ({ page, request }) => {
+  const taskName = "Ler um livro de TypeScript";
+  await request.delete("http://localhost:3333/helper/tasks/" + taskName);
   await page.goto("http://localhost:3000");
 
   const inputTaskName = page.locator("input[type=text]");
-  await inputTaskName.fill(faker.lorem.words());
+  await inputTaskName.fill(taskName);
 
   await page.click("css=button >> text=Create");
+
+  const target = page.locator(`css=.task-item p >> text=${taskName}`);
+  await expect(target).toBeVisible();
 });
